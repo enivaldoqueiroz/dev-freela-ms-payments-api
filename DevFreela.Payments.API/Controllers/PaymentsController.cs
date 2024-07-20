11163092848678dd4db0c381805312b9f99f20ca
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-07-20 12:15:16 -0300

    Add project files.

---
+++ DevFreela.Payments.API/Controllers/PaymentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.Payments.API.Controllers
+{
+    [Route("api/payments")]
+    [ApiController]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IPaymentService _paymentsService;
+    }
+}
