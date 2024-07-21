a06e3e8d6b78ed85ee1a460ab994f6d4eaf06569
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-07-21 11:42:57 -0300

    Adição do pacote RabbitMQ.Client. Criação da classe ProcessPaymentConsumer para consumir a fila nas mensagens no RabbitMQ

---
+++ DevFreela.Payments.API/Model/PaymentApprovedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace DevFreela.Payments.API.Model
+{
+    public class PaymentApprovedIntegrationEvent
+    {
+        public PaymentApprovedIntegrationEvent(int idProject)
+        {
+            IdProject = idProject;
+        }
+
+        public int IdProject { get; set; }
+    }
+}
